c4069dc122034affbe7e467d972a9c3469b74942
Nafiz Ahmed <mehnafiz@gmail.com>
2026-07-15 09:53:23 +0600

    update theme error

--- MID/Mid_final_task/protfolio_web/js/theme.js
+++ MID/Mid_final_task/protfolio_web/js/theme.js
@@ -1,10 +1,26 @@
-function toggleTheme() {
-    document.body.classList.toggle("dark");
+let theme = localStorage.getItem("theme") || "light";
 
+function applyTheme() {
     let btn = document.getElementById("themeBtn");
-    if (document.body.classList.contains("dark")) {
+
+    if (theme === "dark") {
+        document.body.classList.add("dark");
         btn.textContent = "Light Mode";
     } else {
+        document.body.classList.remove("dark");
         btn.textContent = "Dark Mode";
     }
 }
+
+function toggleTheme() {
+    if (theme === "dark") {
+        theme = "light";
+    } else {
+        theme = "dark";
+    }
+
+    localStorage.setItem("theme", theme);
+    applyTheme();
+}
+
+applyTheme();
